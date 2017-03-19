772db0e4353a8a20025cac460bb413bd618b5163
Anderson Carvalho <anderson.forfan@gmail.com>
2017-03-19 19:18:46 -0300

    novas atributos

--- Models/user.js
+++ Models/user.js
@@ -4,7 +4,10 @@ var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
     username : String,
-    password : String
+    password : String,
+    age: { type: Number, min: 18, index: true },
+    bio: { type: String, match: /[a-z]/ },
+    dateCreate : { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 var app = require('./Config/app_config');
 var db = require('./Config/db_config');
 
-app.get('/', function(req, resp){
+app.get('/', function(req, res){
 
     console.log('Version: '+ process.version);
     console.log(process.versions);
 
+    var testUpcase = 'anderson'
+    testUpcase = testUpcase[0].toUpperCase() + testUpcase.slice(1);
+
     var pessoa = {
-              nome : 'Anderson de Carvalho'
+              nome : 'Anderson de Carvalho',
+              date : new Date('1969-12-29 22:07:44'),
+              maiuscula : testUpcase
           };
 
-    resp.json(pessoa);
+    res.json(pessoa);
 });
